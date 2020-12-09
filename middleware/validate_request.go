4c063c6b9bd4b2688cd0d5c6240bcb0a631bcaad
hierobs <hieronimus.budisantosa@gmail.com>
2020-12-09 08:24:56 +0700

    add validate middleware

---
+++ middleware/validate_request.go
@@ -0,0 +1,29 @@
+package validatereqmid
+
+import (
+	"github.com/gin-gonic/gin"
+	myjwt "github.com/hieronimusbudi/go-bookstore-utils/my_jwt"
+	resterrors "github.com/hieronimusbudi/go-bookstore-utils/rest_errors"
+)
+
+func ValidateRequest(jwtSecret string, jwtCookieName string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get token from cookie
+		jwtCookie, jwtErr := c.Request.Cookie(jwtCookieName)
+		if jwtErr != nil {
+			restJwtErr := resterrors.NewUnauthorizedError(jwtErr.Error())
+			c.JSON(restJwtErr.Status(), restJwtErr)
+			return
+		}
+
+		// Validate token
+		tokenClaims, tokenErr := myjwt.ValidateToken(jwtCookie.Value, jwtSecret)
+		if tokenErr != nil {
+			c.JSON(tokenErr.Status(), tokenErr)
+			return
+		}
+
+		c.Set("tokenClaims", tokenClaims)
+		c.Next()
+	}
+}
