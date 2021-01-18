1d29d1c0bbf1be29a44267a14f48b16a80c9049e
hierobs <hieronimus.budisantosa@gmail.com>
2021-01-18 17:05:32 +0700

    fix event

--- events/item_created_event.go
+++ events/item_created_event.go
@@ -1,11 +1,13 @@
 package eventsutils
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type ItemCreatedContext struct {
-	ID                string  `json:"id"`
-	Seller            int64   `json:"seller"`
-	Title             string  `json:"title"`
-	Price             float32 `json:"price"`
-	AvailableQuantity int     `json:"available_quantity"`
-	SoldQuantity      int     `json:"sold_quantity"`
-	Status            string  `json:"status"`
+	ID                primitive.ObjectID `json:"id" bson:"_id"`
+	Seller            int64              `json:"seller" bson:"seller"`
+	Title             string             `json:"title" bson:"title"`
+	Price             float32            `json:"price" bson:"price"`
+	AvailableQuantity int                `json:"available_quantity" bson:"available_quantity"`
+	SoldQuantity      int                `json:"sold_quantity" bson:"sold_quantity"`
+	Status            string             `json:"status" bson:"status"`
 }
